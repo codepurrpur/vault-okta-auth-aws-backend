66a5573c9cd71d576e68408bbb5c99e366955d31
Chris Zhang <chris.zhang.syd@gmail.com>
2023-05-26 17:59:33 +1000

    Updates

--- consumer-workspace/aws.tf
+++ consumer-workspace/aws.tf
@@ -1,14 +1,3 @@
-data "vault_aws_access_credentials" "creds" {
-  backend = var.aws_backend
-  role    = var.aws_role
-}
-
-provider "aws" {
-  access_key = data.vault_aws_access_credentials.creds.access_key
-  secret_key = data.vault_aws_access_credentials.creds.secret_key
-  region     = var.region
-}
-
 resource "random_id" "id" {
   byte_length = 4
   prefix      = "cz-"
--- consumer-workspace/provider.tf
+++ consumer-workspace/provider.tf
@@ -1,2 +1,13 @@
+data "vault_aws_access_credentials" "creds" {
+  backend = var.aws_backend
+  role    = var.aws_role
+}
+
+provider "aws" {
+  access_key = data.vault_aws_access_credentials.creds.access_key
+  secret_key = data.vault_aws_access_credentials.creds.secret_key
+  region     = var.region
+}
+
 provider "vault" {
 }
